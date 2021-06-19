a4118868c0594e57fb636826152fb71c9a4fed8e
startbug <51115577+startbug@users.noreply.github.com>
2021-06-19 18:53:46 +0800

    异步消息确认添加确认回调

--- rabbitmq-hello/src/main/java/com/starbug/rabbitmq/four/ConfirmMessage.java
+++ rabbitmq-hello/src/main/java/com/starbug/rabbitmq/four/ConfirmMessage.java
@@ -6,6 +6,8 @@ import com.starbug.rabbitmq.utils.RabbitMQUtils;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 /**
  *  @Author Starbug
@@ -73,7 +75,7 @@ public class ConfirmMessage {
         System.out.println("发布1000个批量确认消息,耗时:" + (end - start) + "ms");
     }
 
-    private static void publishMessageAsync() throws IOException, InterruptedException {
+    private static void publishMessageAsync() throws IOException {
         Channel channel = RabbitMQUtils.getChannel();
         //声明队列
         String queueName = UUID.randomUUID().toString();
@@ -81,13 +83,31 @@ public class ConfirmMessage {
         //开启发布确认
         channel.confirmSelect();
 
+        ConcurrentSkipListMap<Long, String> concurrentSkipListMap = new ConcurrentSkipListMap();
+
+        /**
+         * 线程安全有序的一个哈希表，适用于高并发的场景
+         * 1.将序号和消息进行关联
+         * 2.批量删除条目，只要给到序号
+         * 3.支持高并发（多线程）
+         **/
         //添加监听器,进行消息的确认
         channel.addConfirmListener((deliveryTag, multiple) -> {
+            if (multiple) {
+                //批量应答，则全部清除
+                ConcurrentNavigableMap<Long, String> confirmed =
+                        concurrentSkipListMap.headMap(deliveryTag);
+                confirmed.clear();
+            } else {
+                //清理当前这条消息
+                concurrentSkipListMap.remove(deliveryTag);
+            }
             //监听发送成功的消息
             System.out.println("确认的消息:" + deliveryTag);
         }, (deliveryTag, multiple) -> {
+            String message = concurrentSkipListMap.get(deliveryTag);
             //监听发送失败的消息
-            System.out.println("未确认的消息:" + deliveryTag);
+            System.out.println("未确认的消息:" + deliveryTag + " 消息为:" + message);
         });
 
         long start = System.currentTimeMillis();
@@ -95,6 +115,8 @@ public class ConfirmMessage {
         for (int i = 1; i <= MESSAGE_COUNT; i++) {
             String message = String.valueOf(i);
             channel.basicPublish("", queueName, null, message.getBytes(StandardCharsets.UTF_8));
+            //1.此处记录下所有要发送的消息，消息综合
+            concurrentSkipListMap.put(channel.getNextPublishSeqNo(), message);
         }
 
         long end = System.currentTimeMillis();

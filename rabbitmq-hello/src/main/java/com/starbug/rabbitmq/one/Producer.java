17941f966d9dad22db68f66260c0db1907453373
startbug <51115577+startbug@users.noreply.github.com>
2021-07-01 23:03:28 +0800

    添加消息优先级案例

--- rabbitmq-hello/src/main/java/com/starbug/rabbitmq/one/Producer.java
+++ rabbitmq-hello/src/main/java/com/starbug/rabbitmq/one/Producer.java
@@ -1,16 +1,19 @@
 package com.starbug.rabbitmq.one;
 
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 /**
- *  @Author Starbug
- *  @Date 2021/6/17 22:27
+ * @Author Starbug
+ * @Date 2021/6/17 22:27
  */
 public class Producer {
 
@@ -35,7 +38,10 @@ public class Producer {
          * 3.该队列是否只提供一个消费者进行消费,是否进行消息共享,true可以多个消费者消费,false只能一个消费者消费
          * 4.是否自动删除,最后一个消费者断开连接后,该队列是否自动删除,true自动删除,false亦然
          **/
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        //为队列设置优先级范围0-10(官方允许范围为0-255),不适宜设置过大,浪费CPU和内存
+        Map<String, Object> arguments = new HashMap<>();
+        arguments.put("x-max-priority", 10);
+        channel.queueDeclare(QUEUE_NAME, false, false, false, arguments);
         //发消息
         String message = "hello world";
         /**
@@ -45,6 +51,15 @@ public class Producer {
          * 3.其他参数信息
          * 4.发送的消息
          **/
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
+                AMQP.BasicProperties properties = new AMQP.BasicProperties().builder().priority(5).build();
+                //info5这条消息优先被消费(优先级为5)
+                channel.basicPublish("", QUEUE_NAME, properties, ("info" + i).getBytes(StandardCharsets.UTF_8));
+            } else {
+                channel.basicPublish("", QUEUE_NAME, null, ("info" + i).getBytes(StandardCharsets.UTF_8));
+            }
+        }
         channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
         System.out.println("发送完成");
     }
